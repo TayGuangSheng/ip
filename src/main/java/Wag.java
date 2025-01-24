827f38ac51646738cfe89d034f4f0e2b14001ca3
TayGuangSheng <tayguangsheng@gmail.com>
2025-01-24 11:38:38 +0800

    Create chatbot Wag

---
+++ src/main/java/Wag.java
@@ -0,0 +1,17 @@
+public class Wag {
+    public static void main(String[] args) {
+        String logo = "\n" +
+                " __      __                 \n" +
+                "/  \\    /  \\_____     ____  \n" +
+                "\\   \\/\\/   /\\__  \\   / ___\\ \n" +
+                " \\        /  / __ \\_/ /_/  >\n" +
+                "  \\__/\\  /  (____  /\\___  / \n" +
+                "       \\/        \\//_____/  \n";
+        System.out.println(logo);
+        System.out.println("_______________________________________");
+        System.out.println("Hello! I'm Wag");
+        System.out.println("What can I do for you?");
+        System.out.println("_______________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
