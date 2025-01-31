f79446a47122cc39ac1c47ce10aa814b96bedc1d
TayGuangSheng <tayguangsheng@gmail.com>
2025-01-31 15:46:05 +0800

    echo implementation

--- src/main/java/Wag.java
+++ src/main/java/Wag.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Wag {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = "\n" +
                 " __      __                 \n" +
                 "/  \\    /  \\_____     ____  \n" +
@@ -7,11 +10,24 @@ public class Wag {
                 " \\        /  / __ \\_/ /_/  >\n" +
                 "  \\__/\\  /  (____  /\\___  / \n" +
                 "       \\/        \\//_____/  \n";
+
         System.out.println(logo);
         System.out.println("_______________________________________");
         System.out.println("Hello! I'm Wag");
         System.out.println("What can I do for you?");
         System.out.println("_______________________________________");
+
+        String command = "";
+
+        // Loop to handle user input and echo the command until "bye" is typed
+        while (!command.equalsIgnoreCase("bye")) {
+            command = scanner.nextLine();  // Get user input
+            System.out.println("_______________________________________");
+            System.out.println(command);  // Echo the command
+            System.out.println("_______________________________________");
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_______________________________________");
     }
 }
