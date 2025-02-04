eba84efa1d9585e568ed6965e29cf729d819b965
TayGuangSheng <tayguangsheng@gmail.com>
2025-02-05 00:38:04 +0800

    Level 4: Implement To Do, Event, Deadline

--- src/main/java/Wag.java
+++ src/main/java/Wag.java
@@ -1,5 +1,82 @@
 import java.util.Scanner;
 
+// Abstract class to represent a generic task
+abstract class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    // Override toString to return the task status and description
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
+
+// Class for Todo tasks
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    // Override toString to represent Todo tasks with a [T] icon
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+// Class for Deadline tasks
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    // Override toString to represent Deadline tasks with a [D] icon and deadline date
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+// Class for Event tasks
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    // Override toString to represent Event tasks with a [E] icon and time range
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
+// Main class for the Wag Program
 public class Wag {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,40 +86,41 @@ public class Wag {
                 "\\   \\/\\/   /\\__  \\   / ___\\ \n" +
                 " \\        /  / __ \\_/ /_/  >\n" +
                 "  \\__/\\  /  (____  /\\___  / \n" +
-                "       \\/        \\//_____/  \n";
-
+                "       \\/        \\/_____/  \n";
         System.out.println(logo);
         System.out.println("_______________________________________");
         System.out.println("Hello! I'm Wag");
         System.out.println("What can I do for you?");
         System.out.println("_______________________________________");
 
-        String[] tasks = new String[100];  // Store up to 100 tasks
-        boolean[] taskStatus = new boolean[100];  // Store task completion status
-        int taskCount = 0;  // Count of tasks added
-        String command = "";
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+        String command;
 
-        // Loop to handle user input and echo the command until "bye" is typed
-        while (!command.equalsIgnoreCase("bye")) {
-            command = scanner.nextLine();  // Get user input
+        while (true) {
+            command = scanner.nextLine();
             System.out.println("_______________________________________");
 
-            if (command.equalsIgnoreCase("list")) {
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_______________________________________");
+                break;
+            } else if (command.equalsIgnoreCase("list")) {
                 if (taskCount == 0) {
                     System.out.println("No tasks added yet.");
                 } else {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        String status = taskStatus[i] ? "[X]" : "[ ]";  // Mark task as done or not
-                        System.out.println((i + 1) + ". " + status + " " + tasks[i]);
+                        System.out.println((i + 1) + ". " + tasks[i]);
                     }
                 }
             } else if (command.startsWith("mark")) {
                 try {
                     int taskId = Integer.parseInt(command.split(" ")[1]) - 1;
                     if (taskId >= 0 && taskId < taskCount) {
-                        taskStatus[taskId] = true;  // Mark task as done
-                        System.out.println("Nice! I've marked this task as done:\n  [X] " + tasks[taskId]);
+                        tasks[taskId].markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks[taskId]);
                     } else {
                         System.out.println("Invalid task number.");
                     }
@@ -53,25 +131,37 @@ public class Wag {
                 try {
                     int taskId = Integer.parseInt(command.split(" ")[1]) - 1;
                     if (taskId >= 0 && taskId < taskCount) {
-                        taskStatus[taskId] = false;  // Mark task as not done
-                        System.out.println("OK, I've marked this task as not done yet:\n  [ ] " + tasks[taskId]);
+                        tasks[taskId].unmarkAsDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks[taskId]);
                     } else {
                         System.out.println("Invalid task number.");
                     }
                 } catch (Exception e) {
                     System.out.println("Invalid command format. Please use 'unmark <task_number>'.");
                 }
-            } else if (!command.equalsIgnoreCase("bye")) {
-                tasks[taskCount] = command;  // Add task to the list
-                taskStatus[taskCount] = false;  // Default status is not done
-                taskCount++;  // Increment task count
-                System.out.println("added: " + command);
+            } else if (command.startsWith("todo")) {
+                String taskDescription = command.substring(5);
+                tasks[taskCount] = new Todo(taskDescription);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+            } else if (command.startsWith("deadline")) {
+                String[] parts = command.substring(9).split(" /by ", 2);
+                tasks[taskCount] = new Deadline(parts[0], parts[1]);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+            } else if (command.startsWith("event")) {
+                String[] parts = command.substring(6).split(" /from | /to ", 3);
+                tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+            } else {
+                System.out.println("Invalid command.");
             }
-
             System.out.println("_______________________________________");
         }
-
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_______________________________________");
     }
 }
